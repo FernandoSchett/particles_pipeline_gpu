619048acec2245ffd39b629f09a06717969d1d53
marquesschettini1 <fernando.schettini@aln.senaicimatec.edu.br>
2025-08-21 17:04:01 +0200

    trying to make a slurm script

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -3,12 +3,16 @@
 #SBATCH --output=nandos_cool_merg_algthm_exp.out     
 #SBATCH --error=nandos_cool_merg_algthm_exp.err     
 #SBATCH --nodes=1                 
-#SBATCH --ntasks=128             
+#SBATCH --ntasks=256             
 #SBATCH --cpus-per-task=1         
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=masid        
+#SBATCH --nodelist=jrc[0737-0832]
 
 source ./load_modules.sh
+
+AVAILABLE=$(nproc)
+echo "Available processes: $AVAILABLE."
 
 cd "$(dirname "$0")/.."
 
@@ -20,7 +24,7 @@ cd build
 cmake ..
 cmake --build .
 
-for np in 4 8 16
+for np in 4 8 16 32 64 128 156
 do
     for i in 1 2 3 4 5
     do
