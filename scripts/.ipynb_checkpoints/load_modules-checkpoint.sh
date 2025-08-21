3a07db389ec25e1ab965188c5c7ecd770a513a9b
marquesschettini1 <marquesschettini1@jrlogin05.jureca>
2025-08-21 16:43:22 +0200

    slurm file added

--- scripts/.ipynb_checkpoints/load_modules-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-module load Boost/1.86.0 GCC OpenMPI CMake
--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+#SBATCH --job-name=nandos_cool_merg_algthm_exp       
+#SBATCH --output=nandos_cool_merg_algthm_exp.out     
+#SBATCH --error=nandos_cool_merg_algthm_exp.err     
+#SBATCH --nodes=1                 
+#SBATCH --ntasks=128             
+#SBATCH --cpus-per-task=1         
+#SBATCH --time=01:00:00
+#SBATCH --partition=masid        
+
+source ./load_modules.sh
 
 cd "$(dirname "$0")/.."
 
 rm -rf build
+rm -f results.csv
+mkdir build
+cd build
 
-rm results.csv
-
-mkdir build 
-
-cd build 
-
-cmake .. 
-
+cmake ..
 cmake --build .
 
-for np in 4 8 16 32 64 128
+for np in 4 8 16
 do
     for i in 1 2 3 4 5
     do
         echo "Executing with $np processes..."
-        mpirun -np $np ./p_sfc_exe 8
+        srun -n $np ./p_sfc_exe 8
     done
 done
