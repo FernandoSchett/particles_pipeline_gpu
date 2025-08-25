501447128526bb3428137855d879b8137bd46368
Fernando Antonio Marques Schettini <fernando.schettini@aln.senaicimatec.edu.br>
2025-08-25 09:52:09 +0200

    small changes at scripts

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -7,6 +7,6 @@ echo "Running with $NP processes"
 
 for i in 1 2 3 4 5
 do
-    echo "Execution #$i with $NP processes..."
-    srun -n $NP ../build/p_sfc_exe 2
+    echo "Execution #$i with $NP processes and $2 power particles..."
+    srun -n $NP ../build/p_sfc_exe $2
 done
--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -13,20 +13,23 @@ cd build
 cmake ..
 cmake --build .
 
-for np in 1 2 4 8 16 32 64 128 256
-do
-    NODES=$(( (np + CORES_PER_NODE - 1) / CORES_PER_NODE ))
-    echo "Submitting job: np=$np, nodes=$NODES, partition=$PARTITION"
-    
-    sbatch \
-        --nodes=$NODES \
-        --ntasks=$np \
-        --cpus-per-task=1 \
-        --time=00:10:00 \
-        --partition=$PARTITION \
-        --account=gsp25 \
-        --job-name=exp_np${np} \
-        --output=exp_np${np}_%j.out \
-        --error=exp_np${np}_%j.err \
-        ../scripts/run_experiment.sh $np
+for pp in 8 
+done
+    for np in 1 2 4 8 16 32 64 128 256
+    do
+        NODES=$(( (np + CORES_PER_NODE - 1) / CORES_PER_NODE ))
+        echo "Submitting job: np=$np, nodes=$NODES, partition=$PARTITION"
+        
+        sbatch \
+            --nodes=$NODES \
+            --ntasks=$np \
+            --cpus-per-task=1 \
+            --time=00:10:00 \
+            --partition=$PARTITION \
+            --account=gsp25 \
+            --job-name=exp_np${np} \
+            --output=exp_np${np}_%j.out \
+            --error=exp_np${np}_%j.err \
+            ../scripts/run_experiment.sh $np $pp
+    done
 done
